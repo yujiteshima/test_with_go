2b4a09e12784377d4e023345a97b72c3ef831c62
Yuji Teshima <yuji.teshima@gmail.com>
2020-06-12 18:18:58 +0900

    complete adder

--- integers/adder.go
+++ integers/adder.go
@@ -1,14 +1,6 @@
 package integers
 
-import "fmt"
-
 // Add takes two integers and return the sum of them.
 func Add(x, y int) int {
 	return x + y
-}
-
-func ExampleAdd() {
-	sum := Add(1, 5)
-	fmt.Println(sum)
-	// Output: 6
 }
